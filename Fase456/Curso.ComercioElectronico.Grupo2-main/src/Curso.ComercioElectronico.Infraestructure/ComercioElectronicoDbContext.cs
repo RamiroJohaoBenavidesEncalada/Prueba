df14eb1619c5af8bd6bcaafd1ce7c77d77953b67
RamiroJohaoBenavidesEncalada <gokubena777@gmail.com>
2022-10-31 09:42:13 -0500

    Version 456

---
+++ Fase456/Curso.ComercioElectronico.Grupo2-main/src/Curso.ComercioElectronico.Infraestructure/ComercioElectronicoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Curso.ComercioElectronico.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Options;
+
+namespace Curso.ComercioElectronico.Infraestructure;
+
+
+public class ComercioElectronicoDbContext : DbContext, IUnitOfWork
+{
+
+    //Agregar sus entidades
+    public DbSet<Marca> Marcas { get; set; }
+
+    public DbSet<TipoProducto> TipoProductos { get; set; }
+    
+    public DbSet<Producto> Productos { get; set; }
+   
+    public string DbPath { get; set; }
+
+    public ComercioElectronicoDbContext(DbContextOptions<ComercioElectronicoDbContext> options) : base(options)
+    {
+    } 
+
+}
+
+
+
---
+++ Fase456/Curso.ComercioElectronico.Grupo2-main/src/Curso.ComercioElectronico.Infraestructure/MarcaRepository.cs
@@ -0,0 +1,64 @@
+
+using Curso.ComercioElectronico.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso.ComercioElectronico.Infraestructure;
+
+public class MarcaRepository : EfRepository<Marca>, IMarcaRepository
+{
+    public MarcaRepository(ComercioElectronicoDbContext context) : base(context)
+    {
+    }
+
+    public async Task<bool> ExisteNombre(string nombre) {
+
+        var resultado = await this._context.Set<Marca>()
+                       .AnyAsync(x => x.Nombre.ToUpper() == nombre.ToUpper());
+
+        return resultado;
+    }
+
+    public async Task<bool> ExisteNombre(string nombre, int idExcluir)  {
+
+        var query =  this._context.Set<Marca>()
+                       .Where(x => x.Id != idExcluir)
+                       .Where(x => x.Nombre.ToUpper() == nombre.ToUpper())
+                       ;
+
+        var resultado = await query.AnyAsync();
+
+        return resultado;
+    }
+
+    
+}
+
+
+public class TipoProductoRepository : EfRepository<TipoProducto>, ITipoProductoRepository
+{
+    public TipoProductoRepository(ComercioElectronicoDbContext context) : base(context)
+    {
+    }
+
+    public async Task<bool> ExisteNombre(string nombre) {
+
+        var resultado = await this._context.Set<TipoProducto>()
+                       .AnyAsync(x => x.Nombre.ToUpper() == nombre.ToUpper());
+
+        return resultado;
+    }
+
+    public async Task<bool> ExisteNombre(string nombre, int idExcluir)  {
+
+        var query =  this._context.Set<TipoProducto>()
+                       .Where(x => x.Id != idExcluir)
+                       .Where(x => x.Nombre.ToUpper() == nombre.ToUpper())
+                       ;
+
+        var resultado = await query.AnyAsync();
+
+        return resultado;
+    }
+
+    
+}
